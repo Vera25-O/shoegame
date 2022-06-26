e4826fa32e68bf93e391c6b7cbdd10940f7a7dd7
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-26 21:10:31 +0300

    update project structure and refactor  image references

--- index.js
+++ index.js
@@ -1,56 +1,48 @@
-
-function displayImageAndDetails(){
-    const imageDetails = fetch (" http://localhost:3000/shoes/1")
-    .then(resp => resp.json())
-    .then(data => renderImageAndDetails(data))
-    return imageDetails
-}
-displayImageAndDetails()
-
-let imgName = document.getElementById('image-name')
-let imgSrc = document.getElementById('card-image')
-let imgLikesCount = document.getElementById('like-count')
-
-
-
-
-function renderImageAndDetails(imageDetails){
-  imgName.textContent = imageDetails.name;
-  imgSrc.src = imageDetails.image;
-  imgLikesCount.textContent =`${imageDetails.likes} likes`;
- 
+(function renderImageAndDetails() {
+  // 1. get a reference to elements on page
+  // TODO: should these elements have default content like image, text, etc?
+  const imgName = document.getElementById('image-name')
+  const imgSrc = document.getElementById('card-image')
+  const imgLikesCount = document.getElementById('like-count')
 
   
-}
+  // 2. fetch data from api
+  // TODO: should we do an if check here, to make sure we have `imgName`, `imgSrc` and `imgLikesCount`, before we attempt to fetch data
+  fetch(" http://localhost:3000/shoes/1")
+    .then(resp => resp.json())
+    .then(imageDetails => {
 
-
-
-
+      // render data on elements
+      imgName.textContent = imageDetails.name;
+      imgSrc.src = imageDetails.image;
+      imgLikesCount.textContent = `${imageDetails.likes} likes`;
+    })
+})() // IIFE - https://developer.mozilla.org/en-US/docs/Glossary/IIFE
 
 
 // like button 
-function dom(){
-const toggleHeart = () => {
-  const likeButton = document.querySelector("#like-button")
-    
-  likeButton.addEventListener("click", (event)  =>{
- if(likeButton.textContent === "♥"){
-    likeButton.textContent = "♡"
- } else {
-    likeButton.textContent = "♥";
-    likeCounter();
- }
- })
-}
+function dom() {
+  const toggleHeart = () => {
+    const likeButton = document.querySelector("#like-button")
 
-let n=0
-const likeCounter = () => {
- const   likeCount = document.querySelector("#like-count");
- n++;
- likeCount.textContent = `${n} likes`
-}
+    likeButton.addEventListener("click", (event) => {
+      if (likeButton.textContent === "♥") {
+        likeButton.textContent = "♡"
+      } else {
+        likeButton.textContent = "♥";
+        likeCounter();
+      }
+    })
+  }
 
-toggleHeart();
+  let n = 0
+  const likeCounter = () => {
+    const likeCount = document.querySelector("#like-count");
+    n++;
+    likeCount.textContent = `${n} likes`
+  }
+
+  toggleHeart();
 
 }
 
@@ -71,19 +63,19 @@ toggleHeart();
 
 //sliding image
 var images = [
-    "heels.jpeg",
-    "orange.jpeg",
-    "glowy.jpeg"
-    
-  ]
-  var imageHead = document.getElementById("images");
-  var i = 0;
-setInterval(function() {
-      imageHead.style.backgroundImage = "url(" + images[i] + ")";
-      i = i + 1;
-      if (i == images.length) {
-        i =  0;
-      }
+  "images/heels.jpeg",
+  "images/orange.jpeg",
+  "images/glowy.jpeg"
+]
+
+var imageHead = document.getElementById("images");
+var i = 0;
+setInterval(function () {
+  imageHead.style.backgroundImage = "url(" + images[i] + ")";
+  i = i + 1;
+  if (i == images.length) {
+    i = 0;
+  }
 }, 2000);
 
 //text color
@@ -93,16 +85,16 @@ var textColor = document.getElementsByTagName("h1")
 
 function changeColor() {
   --currentColor
-  if (currentColor < 0) currentColor = colors.length -1
+  if (currentColor < 0) currentColor = colors.length - 1
   for (var i = 0; i < textColor.length; i++) {
-    textColor[i].style.color = colors[(currentColor +i) % colors.length]
+    textColor[i].style.color = colors[(currentColor + i) % colors.length]
   }
 }
 
 setInterval(changeColor, 2000)
 
-if(document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded",dom)
-} else{
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", dom)
+} else {
   dom();
 }
